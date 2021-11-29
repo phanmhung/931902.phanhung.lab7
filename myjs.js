236d5c1b99cf9e1f8f8fcd7425cd9fde6d9f547c
Phan M. Hung <pmhungpro1@gmail.com>
2021-11-29 23:41:44 +0700

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,45 @@
+function display(n){
+  var qty=document.getElementById("inputt").value;
+  body=document.body;
+
+  if(n==2){
+    for(var i=1; i<=qty; i++){
+      var div = document.createElement('div');
+      div.className= "triangle";
+      var r = Math.random();
+      div.style.border = r * 400 + "px solid transparent";
+      div.style.borderBottom = r * 400 + "px solid blue";
+      div.style.top= 50 + r * 50 +"px";
+      div.style.left= r *800 +"px";
+
+      body.before(div);
+
+      div.onclick = function(){
+        if(div.style.borderBottomColor !="yellow")
+        div.style.borderBottomColor ="yellow";
+        else this.remove();
+      };
+    }
+  }
+  else {
+    for(var i=1; i<=qty; i++){
+      var div = document.createElement('div');
+      if(n==1) div.className= "square";
+      else div.className= "circle";
+
+      var r = Math.random();
+      div.style.width = r * 300 + "px";
+			div.style.height = r * 300 + "px";
+      div.style.top= 100 + r * 50 +"px";
+      div.style.left= r *1000 +"px";
+
+      body.before(div);
+
+      div.onclick = function(){
+        if(div.style.background!="yellow")
+        div.style.background="yellow";
+        else this.remove();
+      };
+    }
+  }
+}
