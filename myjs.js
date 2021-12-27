41311060634782dad9a3a0a137eb9eb6406594b5
Phan M. Hung <pmhungpro1@gmail.com>
2021-12-27 18:10:26 +0700

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,10 +1,8 @@
-function display(n){
+function triangle(){
   var qty=document.getElementById("inputt").value;
-  body=document.body;
 
-  if(n==2){
-    for(var i=1; i<=qty; i++){
-      var div = document.createElement('div');
+    for(var i=0; i<qty; i++){
+      let div = document.createElement('div');
       div.className= "triangle";
       var r = Math.random();
       div.style.border = r * 400 + "px solid transparent";
@@ -12,32 +10,52 @@ function display(n){
       div.style.top= 50 + r * 50 +"px";
       div.style.left= r *800 +"px";
 
-      body.before(div);
+      document.body.append(div);
 
       div.onclick = function(){
         if(div.style.borderBottomColor =="yellow") this.remove();
         else div.style.borderBottomColor ="yellow";
       };
     }
-  }
-  else {
-    for(var i=1; i<=qty; i++){
-      var div = document.createElement('div');
-      if(n==1) div.className= "square";
-      else div.className= "circle";
+}
+function square(){
+  var qty=document.getElementById("inputt").value;
+  for(var i=0; i<qty; i++){
+    let div = document.createElement('div');
+    div.className= "square";
 
-      var r = Math.random();
+    var r = Math.random();
+    div.style.width = r * 300 + "px";
+    div.style.height = r * 300 + "px";
+    div.style.top= 100 + r * 50 +"px";
+    div.style.left= r *1000 +"px";
+
+    document.body.append(div);
+
+    div.onclick = function(){
+      if(div.style.background =="yellow") this.remove();
+      else div.style.background ="yellow";
+    };
+  }
+}
+
+function circle(){
+  var qty=document.getElementById("inputt").value;
+  for(let i=0; i<qty; i++){
+    let div = document.createElement('div');
+    div.className= 'circle';
+
+    let r = Math.random();
       div.style.width = r * 300 + "px";
 			div.style.height = r * 300 + "px";
       div.style.top= 100 + r * 50 +"px";
       div.style.left= r *1000 +"px";
 
-      body.before(div);
+    document.body.append(div);
 
-      div.onclick = function(){
-        if(div.style.borderBottomColor =="yellow") this.remove();
-        else div.style.borderBottomColor ="yellow";
-      };
-    }
+    div.onclick = function(){
+      if(div.style.background =="yellow") this.remove();
+      else div.style.background ="yellow";
+    };
   }
 }
